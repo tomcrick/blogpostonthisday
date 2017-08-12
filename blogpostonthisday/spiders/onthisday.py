95c243e80f6968438c230fdb1d5599fa66371b56
Tom Crick <tomcrick@gmail.com>
2017-08-12 23:38:22 +0100

    Finds posts in the month

--- blogpostonthisday/spiders/onthisday.py
+++ blogpostonthisday/spiders/onthisday.py
@@ -4,19 +4,38 @@ from calendar import monthrange
 from scrapy.spiders import CrawlSpider
 
 class OnThisDaySpider(CrawlSpider):
+    # spider info
     name = "onthisday_spider"
     allowed_domains = ["proftomcrick.com"]
 
     # URL string with format specifiers
     url_string = ("http://proftomcrick.com/{year}/{month}/")
 
-    # create a list of start urls to crawl formatting the string above
-    # so that correct month end dates are used i.e. 28 for February
-    # on non-leap years
+    # create a list of start URLs to crawl over the entire blog history using current month
     start_urls = []
     today = date.today()
     startyear = 2011
     nextyear = today.year + 1
-    
+
     for year in range(startyear, nextyear):
         start_urls.append(url_string.format(year=year, month=today.month))
+
+
+    # grab info on individual blog posts from this month's summary page
+    def parse(self, response):
+        
+        POST_SELECTOR = '.entry-header'
+        for post in response.css(POST_SELECTOR):
+
+            TITLE_SELECTOR = 'h1 ::text'
+            URL_SELECTOR = 'a::attr(href)'
+            DATE_SELECTOR = './/a/time/@datetime'
+
+            date = (post.xpath(DATE_SELECTOR).extract_first())[0:10]
+            title = post.css(TITLE_SELECTOR).extract_first()
+            url = post.css(URL_SELECTOR).extract_first()
+
+            #print out
+            print(title)
+            print(url)
+            print(date)
